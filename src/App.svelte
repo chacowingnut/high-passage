e5d0df50eedf259ec9b8ddaab24f4b6a4e6194f4
chacowingnut <nathaninkley@gmail.com>
2022-04-19 00:05:38 -0700

    WIP

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let sveeg;
+
+  onMount(() => {
+    panzoom(sveeg);
+  });
+</script>
+
+<svg bind:this={sveeg}>
+  <g id='scene'> 
+    <circle cx='100' cy='100' r='50' fill='pink'></circle>
+  </g>
+</svg>
+
+<style>
+</style>
+
